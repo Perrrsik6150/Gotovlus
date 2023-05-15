daa69560db8fb72ebd9da4a18fdefa8723d62646
Nastya <bobiccom@mail.ru>
2023-05-15 21:53:14 +0300

    newcommit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPriduct (object sender, RoutedEventArgs e)
+        {
+            Produkts produkts = new Produkts();
+            produkts.Owner = this;
+            produkts.Show();
+            this.Hide();
+        }
+
+        private void btnSotrudnici (object sender, RoutedEventArgs e)
+        {
+            Sotrudnici sotrudnici = new Sotrudnici();
+            sotrudnici.Owner = this;
+            sotrudnici.Show();
+            this.Hide();
+        }
+
+        private void btnPokupki (object sender, RoutedEventArgs e)
+        {
+            Pokupki polupki = new Pokupki();
+            polupki.Owner = this;
+            polupki.Show();
+            this.Hide();
+        }
+
+        private void Vixod (object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("Вы уверены, что хотите выйти?", "Внимание!", 
+                MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes) 
+            { 
+             this.Close();
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Windows/Pokupki.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Pokupki.xaml
+    /// </summary>
+    public partial class Pokupki : Window
+    {
+        public Pokupki()
+        {
+            InitializeComponent();
+            PokupkiGrid.ItemsSource = ShopEntities.GetContext().Pokupka.ToList();
+        }
+
+        private void Vernut (object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Owner = this;
+            mainWindow.Show();
+            this.Hide();
+        }
+
+        private void Dobavit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            var pokupkaForRemoving = PokupkiGrid.SelectedItems.Cast<Pokupka>().ToList();
+            if(MessageBox.Show("Вы уверены, что хотите удалить данные?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning)
+                == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ShopEntities.GetContext().Pokupka.RemoveRange(pokupkaForRemoving);
+                    ShopEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    PokupkiGrid.ItemsSource = ShopEntities.GetContext().Pokupka.ToList();
+                }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+
+                }
+            }
+        }
+    }
+}
